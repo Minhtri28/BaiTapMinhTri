5dc0486a5fdba292239608ad9d837f330f699efe
Your Name <you@example.com>
2021-12-08 17:09:48 +0700

    update file

---
+++ index.js
@@ -0,0 +1,5 @@
+var pass;
+let math;
+// chu thich 1 dong
+/* chu thich nhieu dong */
+
---
+++ onclick.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML ="Paragraph Changed";
+}
+var i =10
+var f = 20.5
+var b = true
+var s = 'Ha Noi'
+
+document.write('i = ' + i);
+document.write('<br/>');
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+
+var width = 20;
+       var height = 10;
+       var area = width * height;
+       document,width('Area = ' = area);
